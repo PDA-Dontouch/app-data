94cec2ccee7669d8ce0ea1714e12156659d5093b
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-06-05 14:50:32 +0900

    feat: filter stocks

--- stocks/CrawlingStocks.js
+++ stocks/CrawlingStocks.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+dotenv.config();
+
 const getStockList = async () => {
   try {
     const url = `https://financialmodelingprep.com/api/v3/symbol/available-indexes?apikey=${process.env.FMP_API_KEY}`;
@@ -16,4 +18,39 @@ const getStockList = async () => {
   }
 };
 
-dotenv.config();
+const isStockToUse = (stock) => {
+  if (stock.type != 'stock' && stock.type != 'etf') {
+    return false;
+  }
+
+  if (
+    stock.exchangeShortName != 'NASDAQ' &&
+    stock.exchangeShortName != 'NYSE' &&
+    stock.exchangeShortName != 'KSC'
+  ) {
+    return false;
+  }
+
+  console.log('true: ', stock.type, ' ', stock.exchangeShortName);
+  return true;
+};
+
+const filterStocks = () => {
+  try {
+    const allStocksFile = 'stocks/allStocks.json';
+    const jsonArray = JSON.parse(fs.readFileSync(allStocksFile, 'utf8'));
+
+    const filteredStocks = jsonArray.filter((stock) => isStockToUse(stock));
+
+    fs.writeFileSync(
+      'filteredStocks.json',
+      JSON.stringify(filteredStocks, null, 2)
+    );
+
+    console.log(filteredStocks);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+filterStocks();
