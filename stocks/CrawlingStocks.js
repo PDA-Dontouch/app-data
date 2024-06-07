333b0e04c364a92e5e2f78e056ac3a7b0adcf1c7
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-06-07 16:09:24 +0900

    feat: get stocks growth

--- stocks/CrawlingStocks.js
+++ stocks/CrawlingStocks.js
@@ -38,91 +38,75 @@ const getDividendCalendar = async (start_date, end_date) => {
   }
 };
 
-const getStockDetails = async () => {
+const getStockGrowth = async (nation) => {
   try {
-    const dividendStocksFile = 'stocks/result/dividendStocks.json';
-    const stockDetailsFile = 'stocks/result/stockDetails.json';
+    const stocksFile = `stocks/result/final/${nation}_stocks.json`;
+    const stocksGrowthFile = `stocks/result/final/${nation}_stocks_growth.json`;
 
-    const dividendStocks = JSON.parse(
-      fs.readFileSync(dividendStocksFile, 'utf8')
-    );
-    const stockDetails = JSON.parse(fs.readFileSync(stockDetailsFile, 'utf8'));
+    const stocks = JSON.parse(fs.readFileSync(stocksFile, 'utf8'));
+    const stocksGrowth = JSON.parse(fs.readFileSync(stocksGrowthFile, 'utf8'));
 
-    const stockDetailSymbols = new Set(
-      stockDetails.map((stockDetail) => stockDetail.symbol)
+    const stockSymbolsAlreadyGot = new Set(
+      stocksGrowth.map((stockDetail) => stockDetail.symbol)
     );
 
-    for (const stock of dividendStocks) {
-      if (!stockDetailSymbols.has(stock.symbol)) {
-        const symbol = stock.symbol;
+    for (const stock of stocks) {
+      if (stock.type === 'etf') {
+        continue;
+      }
 
-        console.log('symbol: ', symbol);
+      if (!stockSymbolsAlreadyGot.has(stock.symbol)) {
+        console.log('symbol: ', stock.symbol, ', type: ', stock.type);
 
-        // growth
-        const growthUrl = `https://financialmodelingprep.com/api/v3/financial-growth/${symbol}?period=annual&limit=1&apikey=${process.env.FMP_API_KEY}`;
+        const growthUrl = `https://financialmodelingprep.com/api/v3/financial-growth/${stock.symbol}?period=annual&limit=1&apikey=${process.env.FMP_API_KEY}`;
         const growthResp = await axios.get(growthUrl);
         const growth = growthResp.data[0];
 
-        // score
-        const scoreUrl = `https://financialmodelingprep.com/api/v4/score?symbol=${symbol}&apikey=${process.env.FMP_API_KEY}`;
-        const scoreResp = await axios.get(scoreUrl);
-        const score = scoreResp.data[0];
-
-        // 둘 중 하나라도 null이면 종료
-        if (growthResp == null || scoreResp == null) {
-          // write
-          fs.writeFileSync(
-            'stocks/result/stockDetails.json',
-            JSON.stringify(stockDetails, null, 2)
-          );
-          await delay(60000);
+        if (growth == null) {
+          continue;
         }
 
-        const stockDetail = {
+        const growthToSave = {
           symbol: symbol,
-          market_cap: score.marketCap,
-          altman_z_score: score.altmanZScore,
-          piotroski_score: score.piotroskiScore,
-          ten_y_revenue_growth_per_share: growth.tenYRevenueGrowthPerShare,
-          five_y_revenue_growth_per_share: growth.fiveYRevenueGrowthPerShare,
-          three_y_revenue_growth_per_share: growth.threeYRevenueGrowthPerShare,
+          ten_y_revenue_growth_per_share:
+            growth.tenYRevenueGrowthPerShare || null,
+          five_y_revenue_growth_per_share:
+            growth.fiveYRevenueGrowthPerShare || null,
+          three_y_revenue_growth_per_share:
+            growth.threeYRevenueGrowthPerShare || null,
           ten_y_operating_cf_growth_per_share:
-            growth.tenYOperatingCFGrowthPerShare,
+            growth.tenYOperatingCFGrowthPerShare || null,
           five_y_operating_cf_growth_per_share:
-            growth.fiveYOperatingCFGrowthPerShare,
+            growth.fiveYOperatingCFGrowthPerShare || null,
           three_y_operating_cf_growth_per_share:
-            growth.threeYOperatingCFGrowthPerShare,
-          ten_y_net_income_growth_per_share: growth.tenYNetIncomeGrowthPerShare,
+            growth.threeYOperatingCFGrowthPerShare || null,
+          ten_y_net_income_growth_per_share:
+            growth.tenYNetIncomeGrowthPerShare || null,
           five_y_net_income_growth_per_share:
-            growth.fiveYNetIncomeGrowthPerShare,
+            growth.fiveYNetIncomeGrowthPerShare || null,
           three_y_net_income_growth_per_share:
-            growth.threeYNetIncomeGrowthPerShare,
+            growth.threeYNetIncomeGrowthPerShare || null,
           ten_y_shareholders_equity_growth_per_share:
-            growth.tenYShareholdersEquityGrowthPerShare,
+            growth.tenYShareholdersEquityGrowthPerShare || null,
           five_y_shareholders_equity_growth_per_share:
-            growth.fiveYShareholdersEquityGrowthPerShare,
+            growth.fiveYShareholdersEquityGrowthPerShare || null,
           three_y_shareholders_equity_growth_per_share:
-            growth.threeYShareholdersEquityGrowthPerShare,
+            growth.threeYShareholdersEquityGrowthPerShare || null,
           ten_y_dividend_per_share_growth_per_share:
-            growth.tenYDividendperShareGrowthPerShare,
+            growth.tenYDividendperShareGrowthPerShare || null,
           five_y_dividend_per_share_growth_per_share:
-            growth.fiveYDividendperShareGrowthPerShare,
+            growth.fiveYDividendperShareGrowthPerShare || null,
           three_y_dividend_per_share_growth_per_share:
-            growth.threeYDividendperShareGrowthPerShare,
+            growth.threeYDividendperShareGrowthPerShare || null,
         };
 
-        // push
-        stockDetails.push(stockDetail);
-
-        // test
-        break;
+        stocksGrowth.push(growthToSave);
       }
     }
 
-    // write
     fs.writeFileSync(
-      'stocks/result/stockDetails.json',
-      JSON.stringify(stockDetails, null, 2)
+      `stocks/result/final/${nation}_stocks_growth.json`,
+      JSON.stringify(stocksGrowth, null, 2)
     );
   } catch (err) {
     console.log(err);
@@ -130,4 +114,4 @@ const getStockDetails = async () => {
 };
 
 // getDividendCalendar('2024-01-01', '2024-06-04');
-// getStockDetails();
+getStockGrowth('us');
---
+++ stocks/UpdateStocks.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.STOCK_DB_HOST,
+  user: process.env.STOCK_DB_USER,
+  password: process.env.STOCK_DB_PASSWORD,
+  database: process.env.STOCK_DB_DATABASE,
+});
