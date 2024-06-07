29215cfab878d3b13b700c85fcc087ac443bbef6
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-06-07 13:46:38 +0900

    feat: process stocks to save

--- stocks/SaveStocks.js
+++ stocks/SaveStocks.js
@@ -3,20 +3,43 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 
 dotenv.config();
+let connection;
 
-const stockConnectionConfig = {
-  host: process.env.STOCK_DB_HOST,
-  user: process.env.STOCK_DB_USER,
-  password: process.env.STOCK_DB_PASSWORD,
-  database: table,
+const connect = () => {
+  connection = mysql.createConnection({
+    host: process.env.STOCK_DB_HOST,
+    user: process.env.STOCK_DB_USER,
+    password: process.env.STOCK_DB_PASSWORD,
+    database: process.env.STOCK_DB_DATABASE,
+  });
 };
 
-const saveTest = async () => {
-  try {
-    const stockConnection = await mysql.createConnection(stockConnectionConfig);
+const saveKrStock = (stock) => {
+  const krQuery = `insert into kr_stocks (symbol, name, exchange, type, corp_code) values (?, ?, ?, ?, ?)`;
 
-    const testStock = {};
+  connection.query(
+    krQuery,
+    [stock.symbol, stock.name, stock.exchange, stock.type, stock.corp_code],
+    (err, result) => {
+      if (err) {
+        console.error('Error inserting data:', err);
+        return;
+      }
+      console.log('Data inserted successfully:', result);
+    }
+  );
+};
+
+const saveUsStock = (stock) => {
+  const usQuery = `insert into us_stocks (symbol, name, exchange, type) values (?, ?, ?, ?)`;
+};
+
+const saveStocks = () => {
+  try {
+    connect();
   } catch (err) {
     console.log(err);
   }
 };
+
+saveStocks();
