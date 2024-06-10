f121e03ca4dfb858f271319de92df3c6601628b0
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-06-10 11:38:27 +0900

    feat: json flatify

--- p2p/8percent.mjs
+++ p2p/8percent.mjs
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 import fs from 'fs';
 
 const DEALS_API_URL = "https://events.8percent.kr/api/deals?category=real-estate-special&closed=true&page=1&page_size=50&ordering=-completed_datetime";
@@ -27,40 +26,39 @@ async function fetchDealData(dealId) {
     }
 }
 
-function serializeObject(ob) {
-    const toReturn = {};
+function flattenObject(ob) {
+    const result = {};
 
-    for (let i in ob) {
-        if (!ob.hasOwnProperty(i)) continue;
-
-        if (typeof ob[i] === 'object' && ob[i] !== null) {
-            toReturn[i] = JSON.stringify(ob[i]);
+    function recurse(cur, prop) {
+        if (Object(cur) !== cur) {
+            result[prop] = cur;
+        } else if (Array.isArray(cur)) {
+            for (let i = 0; i < cur.length; i++) {
+                recurse(cur[i], prop + "[" + i + "]");
+            }
+            if (cur.length === 0) result[prop] = [];
         } else {
-            toReturn[i] = ob[i];
+            let isEmpty = true;
+            for (const p in cur) {
+                isEmpty = false;
+                recurse(cur[p], prop ? prop + "_" + p : p);
+            }
+            if (isEmpty && prop) result[prop] = {};
         }
     }
-    return toReturn;
+
+    recurse(ob, "");
+    return result;
 }
 
-async function saveToCsv(dataList, filename) {
+async function saveToJson(dataList, filename) {
     if (dataList.length === 0) {
         console.log("종목이 존재하지 않습니다.");
         return;
     }
 
-    const serializedDataList = dataList.map(data => serializeObject(data));
-    const headers = Object.keys(serializedDataList[0]).map(key => ({ id: key, title: key }));
-    const csvWriter = createCsvWriter({
-        path: filename,
-        header: headers
-    });
-
-    await csvWriter.writeRecords(serializedDataList);
-
-    const BOM = '\uFEFF';
-    const csvData = fs.readFileSync(filename, 'utf8');
-    fs.writeFileSync(filename, BOM + csvData, 'utf8');
-
+    const flattenedDataList = dataList.map(data => flattenObject(data));
+    fs.writeFileSync(filename, JSON.stringify(flattenedDataList, null, 2), 'utf8');
     console.log(`저장 완료`);
 }
 
@@ -76,12 +74,12 @@ async function main() {
         console.log(`${dealId}번 종목 저장 중`);
         const data = await fetchDealData(dealId);
         if (data) {
-            allData.push(data);  
+            allData.push(data);
         }
     }
 
     if (allData.length > 0) {
-        await saveToCsv(allData, "8percent.csv");
+        await saveToJson(allData, "8percent.json");
     }
 }
 
