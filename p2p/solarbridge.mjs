a9a41d2a24262d16020da14997790973274e7f25
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-06-11 15:44:04 +0900

    feat: json field name modification

--- p2p/solarbridge.mjs
+++ p2p/solarbridge.mjs
@@ -5,6 +5,72 @@ import fs from 'fs';
 const urlTemplate = "https://www.solarbridge.kr/product/detail/A000{}";
 const allData = [];
 
+const fieldMapping = {
+    "투자기간": "investment_period",
+    "모집금액": "funding_amount",
+    "자금용도": "fund_usage",
+    "총투자금": "total_investment_amount",
+    "상환방식": "repayment_method",
+    "중도상환수수료": "early_repayment_fee",
+    "사업명": "project_name",
+    "사업부지": "project_site",
+    "설비용량": "facility_capacity",
+    "시공사": "contractor",
+    "사업내용": "business_description",
+    "신용도": "credit_rating",
+    "재무현황": "financial_status",
+    "동일차주 대출현황": "same_borrower_loan_status",
+    "담보물 가치": "collateral_value",
+    "선순위 채권": "senior_debt",
+};
+
+const translateFields = (data) => {
+    const translatedData = {};
+    for (const key in data) {
+        if (fieldMapping[key]) {
+            translatedData[fieldMapping[key]] = removeUnmatchedBrackets(data[key]);
+        } else {
+            translatedData[key] = removeUnmatchedBrackets(data[key]);
+        }
+    }
+    return translatedData;
+};
+
+const removeUnmatchedBrackets = (value) => {
+    const brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    
+    const openBrackets = Object.keys(brackets);
+    const closeBrackets = Object.values(brackets);
+    
+    const stack = [];
+    let cleanedValue = '';
+
+    for (const char of value) {
+        if (openBrackets.includes(char)) {
+            stack.push(char);
+            cleanedValue += char;
+        } else if (closeBrackets.includes(char)) {
+            if (stack.length > 0 && brackets[stack[stack.length - 1]] === char) {
+                stack.pop();
+                cleanedValue += char;
+            }
+        } else {
+            cleanedValue += char;
+        }
+    }
+
+    while (stack.length > 0) {
+        const lastOpenBracket = stack.pop();
+        cleanedValue = cleanedValue.replace(lastOpenBracket, '');
+    }
+
+    return cleanedValue;
+};
+
 const fetchProductDetails = async (i) => {
     const url = urlTemplate.replace("{}", i);
     try {
@@ -25,13 +91,13 @@ const fetchProductDetails = async (i) => {
         const fundingAmount = tableData.eq(2).text().trim().replace(/- /g, '') || "";
 
         const borrowerInfoSection = $('div.border-1.p-40');
-        const borrowerInfoItems = { "대출자 정보 1 제목": "", "대출자 정보 1 내용": "", "대출자 정보 2 제목": "", "대출자 정보 2 내용": "", "대출자 정보 3 제목": "", "대출자 정보 3 내용": "" };
+        const borrowerInfoItems = { "borrower_info_1_title": "", "borrower_info_1_content": "", "borrower_info_2_title": "", "borrower_info_2_content": "", "borrower_info_3_title": "", "borrower_info_3_content": "" };
         borrowerInfoSection.find('div.m-t-20').each((index, element) => {
             const title = $(element).find('div.border-left-4-navy').text().trim().replace(/- /g, '');
             const details = $(element).find('div.p-l-30.c-gray').text().trim().replace(/- /g, '');
             if (title && details) {
-                borrowerInfoItems[`대출자 정보 ${index + 1} 제목`] = title;
-                borrowerInfoItems[`대출자 정보 ${index + 1} 내용`] = details;
+                borrowerInfoItems[`borrower_info_${index + 1}_title`] = title;
+                borrowerInfoItems[`borrower_info_${index + 1}_content`] = details;
             }
         });
 
@@ -52,7 +118,6 @@ const fetchProductDetails = async (i) => {
             }
         });
 
-
         const additionalYieldSection = $('#ivqm_1_focus > div:nth-child(4) > div > div:nth-child(16)');
         const additionalYieldHtml = additionalYieldSection.html().split('<br>').map(text => text.trim().replace(/- /g, ''));
         const additionalYieldItems = {};
@@ -61,15 +126,14 @@ const fetchProductDetails = async (i) => {
             const [key, value] = item.split(':').map(text => text.trim());
             if (key && value) {
                 if (key.includes('수익률(세전)')) {
-                    additionalYieldItems['수익률(세전)'] = value;
+                    additionalYieldItems['gross_return_rate'] = value;
                 } else if (key.includes('순수익률(세후)')) {
-                    additionalYieldItems['순수익률(세후)'] = value;
+                    additionalYieldItems['net_return_rate'] = value;
                 } else if (key.includes('총 예상수익률')) {
-                    additionalYieldItems['총 예상수익률'] = value;
+                    additionalYieldItems['expected_total_return_rate'] = value;
                 }
             }
         });
-
 
         const businessSummarySection = $('#ivqm_2_focus');
         const businessSummaryItems = {};
@@ -90,11 +154,10 @@ const fetchProductDetails = async (i) => {
                         .split('<br>')
                         .flatMap(text => text.split(',').map(item => item.trim()));
                     const admissionItems = {
-                        "인허가 1": admissionHtml[0] || " ",
-                        "인허가 2": admissionHtml[1] || " ",
-                        "인허가 3": admissionHtml[2] || " ",
-                        "인허가 4": admissionHtml[3] || " ",
-                        "인허가 5": admissionHtml[4] || " "
+                        "permit_1": admissionHtml[0] || " ",
+                        "permit_2": admissionHtml[1] || " ",
+                        "permit_3": admissionHtml[2] || " ",
+                        "permit_4": admissionHtml[3] || " ",
                     };
                     Object.assign(businessSummaryItems, admissionItems);
                 }
@@ -107,20 +170,18 @@ const fetchProductDetails = async (i) => {
         });
         
         const businessSummaryImageLinks = {
-            "사업개요 이미지 링크 1": businessSummaryImages[0] || " ",
-            "사업개요 이미지 링크 2": businessSummaryImages[1] || " ",
-            "사업개요 이미지 링크 3": businessSummaryImages[2] || " ",
-            "사업개요 이미지 링크 4": businessSummaryImages[3] || " ",
-            "사업개요 이미지 링크 5": businessSummaryImages[4] || " "
+            "project_overview_image_link_1": businessSummaryImages[0] || " ",
+            "project_overview_image_link_2": businessSummaryImages[1] || " ",
+            "project_overview_image_link_3": businessSummaryImages[2] || " ",
+            "project_overview_image_link_4": businessSummaryImages[3] || " ",
         };
 
         const repaymentResourcesSection = $('#ivqm_3_focus > div.m-t-10.f-14.c-gray-200');
         const repaymentResourcesHtml = repaymentResourcesSection.html().split('<br>').map(text => text.trim().replace(/- /g, ''));
         
         const repaymentResources = {
-            "상환재원 1": repaymentResourcesHtml[0] || " ",
-            "상환재원 2": repaymentResourcesHtml[1] || " ",
-            "상환재원 3": repaymentResourcesHtml[2] || " "
+            "repayment_source_1": repaymentResourcesHtml[0] || " ",
+            "repayment_source_2": repaymentResourcesHtml[1] || " "
         };
 
         const collateralManagementSection = $('#ivqm_4_focus > div:nth-child(3)');
@@ -129,43 +190,41 @@ const fetchProductDetails = async (i) => {
             .flatMap(text => text.split('<br>').map(item => item.trim().replace(/- /g, '')));
         
         const collateralManagement = {
-            "담보 관리 1": collateralManagementHtml[0] || " ",
-            "담보 관리 2": collateralManagementHtml[1] || " ",
-            "담보 관리 3": collateralManagementHtml[2] || " ",
-            "담보 관리 4": collateralManagementHtml[3] || " "
+            "collateral_management_1": collateralManagementHtml[0] || " ",
+            "collateral_management_2": collateralManagementHtml[1] || " ",
+            "collateral_management_3": collateralManagementHtml[2] || " "
         };
 
         const collateralListSection = $('#ivqm_4_focus > div:nth-child(9) > div:nth-child(1) > div.col-sm-8.col-xs-12.xs-padding-5');
         const collateralListHtml = collateralListSection.html().split('<br>').map(text => text.trim().replace(/- /g, ''));
 
         const collateralList = {
-            "담보물 목록 1": collateralListHtml[0] || " ",
-            "담보물 목록 2": collateralListHtml[1] || " ",
-            "담보물 목록 3": collateralListHtml[2] || " ",
-            "담보물 목록 4": collateralListHtml[3] || " ",
-            "담보물 목록 5": collateralListHtml[4] || " "
+            "collateral_list_1": collateralListHtml[0] || " ",
+            "collateral_list_2": collateralListHtml[1] || " ",
+            "collateral_list_3": collateralListHtml[2] || " ",
+            "collateral_list_4": collateralListHtml[3] || " ",
+            "collateral_list_5": collateralListHtml[4] || " "
         };
 
         const collateralRecoveryValueSection = $('#ivqm_4_focus > div:nth-child(9) > div:nth-child(4) > div.col-sm-8.col-xs-12.xs-padding-5');
         const collateralRecoveryValueHtml = collateralRecoveryValueSection.html().split('<br>').map(text => text.trim().replace(/- /g, ''));
         
         const collateralRecoveryValue = {
-            "담보물 회수가액 1": collateralRecoveryValueHtml[0] || " ",
-            "담보물 회수가액 2": collateralRecoveryValueHtml[1] || " ",
-            "담보물 회수가액 3": collateralRecoveryValueHtml[2] || " ",
-            "담보물 회수가액 4": collateralRecoveryValueHtml[3] || " ",
-            "담보물 회수가액 5": collateralRecoveryValueHtml[4] || " "
+            "collateral_recovery_value_1": collateralRecoveryValueHtml[0] || " ",
+            "collateral_recovery_value_2": collateralRecoveryValueHtml[1] || " ",
+            "collateral_recovery_value_3": collateralRecoveryValueHtml[2] || " ",
+            "collateral_recovery_value_4": collateralRecoveryValueHtml[3] || " ",
         };
 
         const creditEnhancementSection = $('#ivqm_4_focus > div:nth-child(5)');
         const creditEnhancementHtml = creditEnhancementSection.html().split('<br>').map(text => text.trim().replace(/- /g, ''));
     
         const creditEnhancement = {
-            "신용 보강안 1": creditEnhancementHtml[0] || " ",
-            "신용 보강안 2": creditEnhancementHtml[1] || " ",
-            "신용 보강안 3": creditEnhancementHtml[2] || " ",
-            "신용 보강안 4": creditEnhancementHtml[3] || " ",
-            "신용 보강안 5": creditEnhancementHtml[4] || " "
+            "credit_enhancement_1": creditEnhancementHtml[0] || " ",
+            "credit_enhancement_2": creditEnhancementHtml[1] || " ",
+            "credit_enhancement_3": creditEnhancementHtml[2] || " ",
+            "credit_enhancement_4": creditEnhancementHtml[3] || " ",
+            "credit_enhancement_5": creditEnhancementHtml[4] || " "
         };
 
         const investorProtectionSection = $('#ivqm_4_focus');
@@ -180,10 +239,10 @@ const fetchProductDetails = async (i) => {
         });
 
         return {
-            "상품명": productName,
-            "연수익률": annualYield,
-            "투자기간": investmentPeriod,
-            "모집금액": fundingAmount,
+            "product_name": productName,
+            "annual_return_rate": annualYield,
+            "investment_period": investmentPeriod,
+            "funding_amount": fundingAmount,
             ...borrowerInfoItems,
             ...productSummaryItems,
             ...additionalYieldItems, 
@@ -202,6 +261,12 @@ const fetchProductDetails = async (i) => {
     }
 };
 
+const saveData = (allData) => {
+    const translatedData = allData.map(translateFields);
+    fs.writeFileSync('solarbridge_product_details.json', JSON.stringify(translatedData, null, 2), 'utf8');
+    console.log("모든 데이터가 파일로 저장되었습니다.");
+};
+
 (async () => {
     for (let i = 400; i <= 500; i++) {
         const data = await fetchProductDetails(i);
@@ -213,7 +278,6 @@ const fetchProductDetails = async (i) => {
     }
 
     if (allData.length > 0) {
-        fs.writeFileSync('solarbridge_product_details.json', JSON.stringify(allData, null, 2), 'utf8');
-        console.log("모든 데이터가 파일로 저장되었습니다.");
+        saveData(allData);
     }
 })();
